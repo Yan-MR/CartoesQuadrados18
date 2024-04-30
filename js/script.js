96668ba1815c475c35ac9c14bcf1f484b10b4a95
YanRodrigues <133587638+Yan-MR@users.noreply.github.com>
2024-04-29 23:27:51 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+document
+  .getElementsByClassName("fab fa-youtube")[0]
+  .addEventListener("click", () => {
+    window.open("https://www.youtube.com/watch?v=_820tw2vXOU");
+  });
+
+document
+  .getElementsByClassName("fab fa-youtube")[1]
+  .addEventListener("click", () => {
+    window.open("https://www.youtube.com/watch?v=ZSiAIVStXl0&t=22s");
+  });
+
+document
+  .getElementsByClassName("fab fa-youtube")[2]
+  .addEventListener("click", () => {
+    window.open("https://www.youtube.com/watch?v=h6ImlCB57sM");
+  });
+
+document.getElementsByClassName("article")[0].addEventListener("click", () => {
+  window.open("https://www.youtube.com/watch?v=msw7K8L_F08");
+});
+
+document.getElementsByClassName("article")[1].addEventListener("click", () => {
+  window.open("https://www.youtube.com/watch?v=eTazPNWhxi4");
+});
+
+document.getElementsByClassName("article")[2].addEventListener("click", () => {
+  window.open("https://www.youtube.com/watch?v=gyJ7grjyCyk");
+});
